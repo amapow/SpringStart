0687ee0dd4a44b70c99478794d1fff3dedbb4985
Janghyeon Jung <amapow25@gmail.com>
2024-01-04 15:35:27 +0900

    Member domain 추가/ MemoryMemberRepository 추가/ Test 작성
    Assertions(assertj)

---
+++ src/test/java/hello/HelloSpring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,60 @@
+package hello.HelloSpring.repository;
+
+import hello.HelloSpring.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+
+
+//    @BeforeEach
+//    void prepare() {
+//        Member member1 = new Member("member1");
+//        Member member2 = new Member("member2");
+//        memberRepository.save(member1);
+//        memberRepository.save(member2);
+//    }
+
+//    @Test
+//    void save() {
+//        Member result = memberRepository.findById(member1.getId()).get();
+//        assertThat(result).isEqualTo(member1);
+//    }
+
+    @AfterEach
+    void clearStore() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void findById() {
+        Member member1 = new Member("member1");
+        memberRepository.save(member1);
+        assertThat(member1).isEqualTo(memberRepository.findById(member1.getId()).get());
+    }
+
+    @Test
+    void findByName() {
+        Member member1 = new Member("member1");
+        memberRepository.save(member1);
+        assertThat(member1).isEqualTo(memberRepository.findByName("member1").get());
+    }
+
+    @Test
+    void findAll() {
+        Member member1 = new Member("member1");
+        memberRepository.save(member1);
+
+        Member member2 = new Member("member1");
+        memberRepository.save(member2);
+
+        assertThat(memberRepository.findAll().size()).isEqualTo(2);
+    }
+}
