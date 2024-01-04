244673d2482732a6cc6df113fc02022d83e9df2f
Janghyeon Jung <amapow25@gmail.com>
2024-01-05 05:16:16 +0900

    memberList.html 추가// Model class에 findMembers()로 memberList를 담아서 보냄// thymeleaf가 Member의 getter를 참조해 값을 가져옴

--- src/main/java/hello/HelloSpring/controller/MemberController.java
+++ src/main/java/hello/HelloSpring/controller/MemberController.java
@@ -4,9 +4,12 @@ import hello.HelloSpring.domain.Member;
 import hello.HelloSpring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -30,5 +33,12 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String memberList(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
