6de412373123b5405db3897730cda73e1264b39f
Janghyeon Jung <amapow25@gmail.com>
2024-01-06 10:47:28 +0900

    h2 database 연동/ 과거 jdbc legacy 코드 추가/ Spring 통합 테스트 추가 @SpringBootTest @Transactional

--- src/test/java/hello/HelloSpring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/HelloSpring/service/MemberServiceIntegrationTest.java
@@ -1,34 +1,34 @@
 package hello.HelloSpring.service;
 
 import hello.HelloSpring.domain.Member;
+import hello.HelloSpring.repository.MemberRepository;
 import hello.HelloSpring.repository.MemoryMemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
-import org.assertj.core.api.Assertions.*;
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
 
-class MemberServiceTest {
-
-    MemberService memberService;
-    MemoryMemberRepository memberRepository;
+    @Autowired MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+    
     @BeforeEach
     public void beforeEach() {
         memberRepository = new MemoryMemberRepository();
         memberService = new MemberService(memberRepository);
     }
 
-    @AfterEach
-    public void afterEach() {
-        memberRepository.clearStore();
-    }
-
     @Test
-    void join() {
+    void 회원가입() {
 
         //given
         Member member = new Member("hello");
@@ -58,18 +58,14 @@ class MemberServiceTest {
     @Test
     void findMembers() {
         //given
-        Member member = new Member("hello");
+        Member member = new Member("hello2");
         memberService.join(member);
 
         //when
         List<Member> members = memberService.findMembers();
 
         //then
-        Assertions.assertThat(members.size()).isEqualTo(1);
+        Assertions.assertThat(members.size()).isEqualTo(3);
 
-    }
-
-    @Test
-    void findOne() {
     }
 }
